61a62bd5e602be0357c6d2b7298aad5cb2d63bb4
sonders <devsonderss@gmail.com>
2022-11-09 12:02:37 -0600

    fin

---
+++ back-end/src/db/migrations/20221103131510_reservationStatus.js
@@ -0,0 +1,13 @@
+const reservationsController = require("../../reservations/reservations.controller");
+
+exports.up = function (knex) {
+  return knex.schema.table("reservations", (table) => {
+    table.string("status").defaultTo("booked");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table("reservations", (table) => {
+    table.dropColumn("status");
+  });
+};
